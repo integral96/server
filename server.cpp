89953613f0efc469032ca849816bf3cdef5cf3c7
Waldemar <41773009+integral96@users.noreply.github.com>
2019-11-10 11:42:30 +0300

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,143 +1,157 @@
 #include <iostream>
 #include <vector>
-#include <memory>
+#include <memory> 
 #include <array>
 #include <string>
 #include <random>
 #include <set>
+#include <utility>
 #include <charconv>
-#include <boost/asio.hpp>
-#include <boost/uuid/uuid.hpp>            
-#include <boost/uuid/uuid_generators.hpp> 
-#include <boost/uuid/uuid_io.hpp>   
+#include <boost/asio.hpp> // подключаем кроссплатформенные библиотеки для сетевой подсистемы
+/* буду использовать TCP протокол, пробовал с UDP но сложноватый логический интерфейс, запутался */
+#include <boost/uuid/uuid.hpp>  //поключаем билиотеку универсальный уникальный идентификатор.          
+#include <boost/uuid/uuid_generators.hpp> //библиотека для генерации имен.
+#include <boost/uuid/uuid_io.hpp>   // обеспечивает перегрузку оператора запись объекта типа
 
+namespace SERVER { //задаем пространство сервер
 
+using namespace boost::asio::ip; //подключаем пространство имен буст/асио
+
+template <typename T = char> // создаем шаблонный класс Биометрический сканер(по умолчанию char)
 class biometric_scanner
 {
     private:
-        std::set<std::pair<char, char>> set_compare;
-        std::unique_ptr<char[]> array;
+        std::set<std::pair<T, T>> set_compare; //создаем множество пар, где будут сравниваться
+                    // поступающие на сканер от клиента биометрические данные в виде массива,
+                    // и массивом биометрический эталон.
+        std::unique_ptr<T[]> array; //создадим массив биометрический эталон
     public:
-        biometric_scanner() {
+        biometric_scanner() { //конструктор по умолчанию 
             this->set_biometric_standard();
         }
-        virtual ~biometric_scanner(){}
+        virtual ~biometric_scanner(){} //деструктор
 
-        void constract(const std::string& ar0)
+        void constract(const std::string& ar0) //заполняем множество парами данных
         {
-            if(ar0.length() > 9 && ar0.length() < 1) throw std::out_of_range("arrays error");
-            
-            for (size_t i = 0; i < ar0.length(); i++)
+            if(ar0.length() > 9 && ar0.length() < 1) throw std::out_of_range("arrays error"); //проверяем на совместимость массива 
+            for (size_t i = 0; i < ar0.length(); i++) //с начальными данными, если не выполняется выкидыываем исключение
             {
-                set_compare.insert(std::make_pair(array[i], ar0[i]));
+                set_compare.insert(std::make_pair(array[i], ar0[i])); //заполняем
             }
         }
-        std::string get_biometric_result()
+        std::string get_biometric_result() //выводим результат, 
         {
-            std::random_device dre;
+            std::random_device dre; //так как в задании надо выводить истину или лож случайным образом, 
             std::mt19937 gen(dre());
-            std::uniform_int_distribution<int> di(0,1);
+            std::uniform_int_distribution<int> di(0,1); //задаем генератор случайных целых чисел 0 и 1
             return di(gen) == 0 ? "true" : "false";
         }
-        void print_biometric()
+        void print_biometric() //распечатываем пару биометрические данные и биометрический эталон
         {
             for(const auto& [x, y] : set_compare) std::cout << "(scan: " << x << ", stand: " << y << "); ";
             std::cout << std::endl;
         }
-        void uuid() const
+        void uuids() const //создаем универсальный уникальный идентификатор
         {
             boost::uuids::uuid uuid = boost::uuids::random_generator()();
-            std::cout << uuid << std::endl;
+            std::cout <<  "UUID Biometric scanner: (" <<uuid << ") ";
         }
         
     private:
-        void set_biometric_standard()
+        void set_biometric_standard() //создаем массив биометрический эталон
         {
-            array = std::make_unique<char[]>(9);
-            for (size_t i = 0; i < 9; i++)
+            array = std::make_unique<T[]>(9); //так как не было огаворены начальные данные
+            for (size_t i = 0; i < 9; i++)    //то я задаю их простым натуральным рядом от 0 до 9(не включительно)
             {
-                std::string s = std::to_string(i);
-                array[i] = *s.c_str();
-            }
+                std::string s = std::to_string(i); //конвертируем в строку
+                array[i] = *s.c_str(); //заполняем массив char
+            }                          //данная функция может ругаться при изменнение типа Т, я не стал дописывать метод по идеологии SFINAE
         }
 };
 
-class sensor
+class sensor //создаем структуру Датчиков
 {
     public:
-        sensor(){}
-        std::string get_sensor_data()
+        sensor(){} //коструктор  по умолчанию
+        std::string get_sensor_data() //выдаем случайным образом Да или Нет, как обговорено в задании
         {
             std::random_device dre;
             std::mt19937 gen(dre());
             std::uniform_int_distribution<int> di(0,1);
             return di(gen) == 0 ? "Yes" : "No";
         }
+        void uuids() const //создаем универсальный уникальный идентификатор для датчиков
+        {
+            boost::uuids::uuid uuid = boost::uuids::random_generator()();
+            std::cout <<  "UUID Sensor: (" <<uuid << ") " << std::endl;
+        }
         virtual ~sensor(){}
 };
-
-// template<typename B = biometric_scanner, typename S = sensor>
+/*создаем класс сессия для класса сервера, он читает данные 
+    из сокета и отправляет обратно ответ в секет*/
 class session : public std::enable_shared_from_this<session>
+/*наследуем класс  который позволяет управляется умным указателем шаред_птр из session
+        который определим ниже, для безопасного создания владельцев объектов session*/
 {
     private:
         std::array<char, 9> array;
-        boost::asio::ip::tcp::socket tcp_socket;
+        tcp::socket tcp_socket; // создаем сокет протокола TCP
         
     public:
-        session(boost::asio::ip::tcp::socket socket):tcp_socket(std::move(socket)) {}
+        session(tcp::socket socket):tcp_socket(std::move(socket)) {} //в коструктор по умолчанию передаем сокет
+                                                                     //с помощью мув семантики.
         virtual ~session(){}
     
-        void read() 
+        void read() //функция где будем использовать метод сокета из буст async_read_some()
         {
-            auto self(shared_from_this());
-            tcp_socket.async_read_some(
-                boost::asio::buffer(array, 9),
-                [this, self](const std::error_code error, const size_t length){
-                    if(!error)
+            auto self(shared_from_this()); //возвращаем умный указатель, который сссылается на this
+            tcp_socket.async_read_some( //считываем:
+                boost::asio::buffer(array, 9), //заполняем буфер массивом
+                [this, self](const std::error_code error, const size_t length){ //передаем лямбду которая захватывает копирывание
+                    if(!error)                                                  //this и указателя, проверяем на ошибки(зависящие от ОП)
                     {
-                        auto number = std::string(array.data(), length);
-                        biometric_scanner bio_scnr;
-                        bio_scnr.constract(number);
-                        sensor snsr;
-                        auto result = bio_scnr.get_biometric_result();
-                        bio_scnr.uuid();
-                        bio_scnr.print_biometric();
+                        auto number = std::string(array.data(), length); //считываем номер из сокета
+                        biometric_scanner bio_scnr; //создаем экземпляр класса биометрический сканер
+                        bio_scnr.constract(number); //передаем ему номер
+                        sensor snsr;                //создаем экземпляр класса Датчик
+                        auto result = bio_scnr.get_biometric_result(); //получаем результат от методов биометрический сканер
+                        bio_scnr.uuids();           //распечатываем UUID сканера
+                        snsr.uuids();               //распечатываем UUID датчика
+                        bio_scnr.print_biometric(); //распечатываем пару биометрический сканер
                         std::cout << " Biometric scanner: " << result << ";\t sensor: " << snsr.get_sensor_data() << std::endl;
-                        write(result);
+                        write(result);              //запишем результат ответ для передачи клиенту
                     }
                 });
         }
     private:
-        void write(std::string_view response) 
+        void write(std::string_view response) //функция для записи ответа клиенту
         {
             auto self(shared_from_this());
-            tcp_socket.async_write_some(
+            tcp_socket.async_write_some( //метод записи эти методы действуюют асиннхронно 
                 boost::asio::buffer(response.data(), response.length()),
-                [this, self](const std::error_code ec, const size_t length){
-                    if(!ec)
-                        read();
+                [this, self](const std::error_code ec, const size_t length){//также создаем лямбду как и выше, 
+                    if(!ec)                                                 //только через лямбду пердаем read()
+                        read();                                             
                 });
         }
 };
-// template<typename B = biometric_scanner, typename S = sensor>
-class server 
+class server //создаем класс сервер
 {
     private:
-        boost::asio::ip::tcp::acceptor tcp_acceptor;
-        boost::asio::ip::tcp::socket tcp_socket;
-        // B _bio_scnr;
-        // S _snsr;
+        tcp::acceptor tcp_acceptor; //создаем ацептор который будет ждать подключение в UDP я такого не нашел и запутался куда передовать подключения.
+        tcp::socket tcp_socket;
+        
     public:
-        server(boost::asio::io_context& context, const int port/* B& bio_scnr, S& snsr*/):tcp_acceptor(context, 
-            boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port)),
-            tcp_socket(context)/*, _bio_scnr(std::move(bio_scnr)), _snsr(std::move(snsr))*/
+        server(boost::asio::io_context& context, const int port):tcp_acceptor(context, //передаем контейнер где будут хранится сигналы строки..
+            tcp::endpoint(tcp::v4(), port)), //создайте endpoint с использованием номера порта. IP-адрес будет любым.
+            tcp_socket(context) //инициализируем сокет
             {
-                std::cout << "Server running on port: " << port << std::endl;
-                accept();
+                std::cout << "Server running on port: " << port << std::endl; //выводим на экран порт
+                accept(); 
             }
         virtual ~server(){}
     private:
-        void accept()
+        void accept() //метод создающий ассинхронно через лямбду проверющию на ошибки умный указатеь на класс сесия, вызывая метод read()
         {
             tcp_acceptor.async_accept(tcp_socket, 
             [this](const std::error_code error){
@@ -148,16 +162,12 @@ class server
         }
 };
 
-
-int main(int argc, char const *argv[])
+void run_server(int port)  //функция запуска сервера
 {
-    int port = std::stoi( argv[1] );
-    biometric_scanner bio_scnr;
-    sensor snsr;
-        try
+    try
         {
-            boost::asio::io_context context;
-            server srv(context, port);
+            boost::asio::io_context context; //создаем объект для ввода вывода
+            server srv(context, port);      //создаем объект класса вервер передаем порт и запускаем.
             context.run();
             
         }
@@ -165,5 +175,15 @@ int main(int argc, char const *argv[])
         {
             std::cerr << e.what() << '\n';
         }
+}
+
+}
+
+int main(int argc, char const *argv[])
+{
+    int port = std::stoi( argv[1] );
+    
+    SERVER::run_server(port);   //Запускам
+
     return 0;
 }
